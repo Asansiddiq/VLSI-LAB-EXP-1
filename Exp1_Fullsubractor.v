70a99b3f0546bc4553658c1eb8717f82dfd09a92
Asansiddiq <160508575+Asansiddiq@users.noreply.github.com>
2024-04-08 10:15:50 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,10 @@
 module fs(a,b,bin,d,bout);
-input a,b,bin;
+input a,b,bin; 
 output d,bout;
-
-
-
+wire w1,w2,w3;
+xor g1(w1,b,bin; 
+xor g2(d,w1,a);
+and g3(w2,a,~w1);
+and g4(w3,~b,bin);
+or g5(bout,w2,w3);
 endmodule
